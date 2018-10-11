852c54ce92630c9648e5dc5441021e8fc31b7392
zhangzjGIT <1094737153@qq.com>
2018-10-11 22:25:52 +0800

    ww

--- src/main/java/com/jk/controller/UserController.java
+++ src/main/java/com/jk/controller/UserController.java
@@ -28,19 +28,15 @@ public class UserController {
         return "login";
     }
 
-    @RequestMapping("getVerify")
-    public void getVerify(HttpServletRequest request, HttpServletResponse response) {
-        try {
-            response.setContentType("image/jpeg");//设置相应类型,告诉浏览器输出的内容为图片
-            response.setHeader("Pragma", "No-cache");//设置响应头信息，告诉浏览器不要缓存此内容
-            response.setHeader("Cache-Control", "no-cache");
-            response.setDateHeader("Expire", 0);
-            RandomValidateCodeUtil randomValidateCode = new RandomValidateCodeUtil();
-            randomValidateCode.getRandcode(request, response);//输出验证码图片方法
-        } catch (Exception e) {
-            /*logger.error("获取验证码失败>>>>   ", e);*/
-        }
-    }
+
+
+
+
+
+
+
+
+
 
     @RequestMapping("userLogin")
     @ResponseBody
